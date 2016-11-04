ea2d235c56d2b99a4828d0e501158abe03598998
Niel <nielmce@gmail.com>
2016-11-04 14:33:11 +0000

    edit url

--- flatpages_demo/urls.py
+++ flatpages_demo/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from hello import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.get_index),
     url(r'^pages/', include('django.contrib.flatpages.urls')),
 ]
